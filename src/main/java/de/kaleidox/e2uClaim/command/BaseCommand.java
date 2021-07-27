c34a3ea0c44b097b6bacb31988e2c7afdf936b2e
Tobias Burdow [Kaleidox] <kaleidox@comroid.org>
2021-07-27 21:45:48 +0200

    Update BaseCommand.java

--- src/main/java/de/kaleidox/e2uClaim/command/BaseCommand.java
+++ src/main/java/de/kaleidox/e2uClaim/command/BaseCommand.java
@@ -1,12 +1,41 @@
 package de.kaleidox.e2uClaim.command;
 
 import de.kaleidox.e2uClaim.E2UClaim;
+import de.kaleidox.e2uClaim.claim.ClaimManager;
+import de.kaleidox.e2uClaim.lock.LockManager;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.comroid.spiroid.command.SpiroidCommand;
+import org.comroid.spiroid.util.BukkitUtil;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.NoSuchElementException;
+
 public enum BaseCommand implements SpiroidCommand {
-    claim(ClaimSubcommand.values()),
+    claim(ClaimSubcommand.values()) {
+        @Override
+        public @Nullable String execute(CommandSender sender, String[] args) {
+            Player player = BukkitUtil.getPlayer(sender).orElseThrow(NoSuchElementException::new);
+            ClaimManager.INSTANCE.requestClaiming(player);
+            return null;
+        }
+    },
+    lock() {
+        @Override
+        public @Nullable String execute(CommandSender sender, String[] args) {
+            Player player = BukkitUtil.getPlayer(sender).orElseThrow(NoSuchElementException::new);
+            LockManager.INSTANCE.requestLock(player);
+            return null;
+        }
+    },
+    unlock() {
+        @Override
+        public @Nullable String execute(CommandSender sender, String[] args) {
+            Player player = BukkitUtil.getPlayer(sender).orElseThrow(NoSuchElementException::new);
+            LockManager.INSTANCE.requestUnlock(player);
+            return null;
+        }
+    },
     e2uClaim(SystemSubcommand.values()) {
         @Override
         public String execute(CommandSender sender, String[] args) {
